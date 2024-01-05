a6aa864aa9a75518a15614985cb4949d5ab2e639
Tru Nguyen <nguyentruhn@gmail.com>
2024-01-05 23:34:02 +0700

    data

---
+++ data.js
@@ -0,0 +1,56 @@
+let myHtml = [
+  {
+    class:"heading",
+    tieude:"1.HTML Heading (Tiêu đề)",
+    noidung:[
+      "Tiêu đề HTML được xác định bằng thẻ h1 đến h6",
+      "kích thước nội dung của thẻ giảm dần từ h1 đến h6"
+    ],
+    vidu:
+      `<iframe height="300" style="width: 100%;" scrolling="no" title="Thể tiêu đề H" src="https://codepen.io/tr-nguyn-vn-the-animator/embed/GReZRMV?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+      See the Pen <a href="https://codepen.io/tr-nguyn-vn-the-animator/pen/GReZRMV">
+      Thể tiêu đề H</a> by trụ nguyễn văn (<a href="https://codepen.io/tr-nguyn-vn-the-animator">@tr-nguyn-vn-the-animator</a>)
+      on <a href="https://codepen.io">CodePen</a>.
+    </iframe>`,
+    viduEdit:"<h1>Heading</h1>"
+  },
+  {
+    class:"paragraph",
+    tieude:"2.HTML Paragraph (Đoạn văn bản)",
+    noidung:[
+      `Phần tử HTML <code>&lt;p&gt;&lt;/p&gt;</code> xác định một đoạn văn.Một đoạn văn luôn bắt đầu trên một dòng mới và trình duyệt sẽ tự động thêm một số khoảng trắng (lề) trước và sau một đoạn văn.`
+    ],
+    vidu:`<iframe height="300" style="width: 100%;" scrolling="no" title="thẻ p" src="https://codepen.io/tr-nguyn-vn-the-animator/embed/zYbqYER?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+    See the Pen <a href="https://codepen.io/tr-nguyn-vn-the-animator/pen/zYbqYER">
+    thẻ p</a> by trụ nguyễn văn (<a href="https://codepen.io/tr-nguyn-vn-the-animator">@tr-nguyn-vn-the-animator</a>)
+    on <a href="https://codepen.io">CodePen</a>.
+  </iframe>`,
+    viduEdit:"<p>paragraph</p>",
+  },
+  {
+    class:"Image",
+    tieude:"2.HTML Paragraph (Đoạn văn bản)",
+    noidung:[
+      `Phần tử HTML <code>&lt;p&gt;&lt;/p&gt;</code> xác định một đoạn văn.Một đoạn văn luôn bắt đầu trên một dòng mới và trình duyệt sẽ tự động thêm một số khoảng trắng (lề) trước và sau một đoạn văn.`
+    ],
+    vidu:`<iframe height="300" style="width: 100%;" scrolling="no" title="Untitled" src="https://codepen.io/tr-nguyn-vn-the-animator/embed/MWxyWbJ?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+    See the Pen <a href="https://codepen.io/tr-nguyn-vn-the-animator/pen/MWxyWbJ">
+    Untitled</a> by trụ nguyễn văn (<a href="https://codepen.io/tr-nguyn-vn-the-animator">@tr-nguyn-vn-the-animator</a>)
+    on <a href="https://codepen.io">CodePen</a>.
+  </iframe>`,
+    viduEdit:"<p>paragraph</p>",
+  },
+  {
+    class:"url",
+    tieude:"2.Thẻ Liên kết (url)",
+    noidung:[
+      `Phần tử HTML <code>&lt;p&gt;&lt;/p&gt;</code> xác định một đoạn văn.Một đoạn văn luôn bắt đầu trên một dòng mới và trình duyệt sẽ tự động thêm một số khoảng trắng (lề) trước và sau một đoạn văn.`
+    ],
+    vidu:`<iframe height="300" style="width: 100%;" scrolling="no" title="Untitled" src="https://codepen.io/tr-nguyn-vn-the-animator/embed/qBvZBmy?default-tab=html%2Cresult" frameborder="no" loading="lazy" allowtransparency="true" allowfullscreen="true">
+    See the Pen <a href="https://codepen.io/tr-nguyn-vn-the-animator/pen/qBvZBmy">
+    Untitled</a> by trụ nguyễn văn (<a href="https://codepen.io/tr-nguyn-vn-the-animator">@tr-nguyn-vn-the-animator</a>)
+    on <a href="https://codepen.io">CodePen</a>.
+  </iframe>`,
+    viduEdit:"<p>paragraph</p>",
+  },
+];
--- main.js
+++ main.js
@@ -1,13 +1,56 @@
-// edit code
-let inputCode = document.querySelectorAll(".input");
-let menuCode = document.querySelectorAll(".menuCode");
+let tagHtml = myHtml.map(function (data) {
+  return `   <div class="${data.class}">
+  <h1 class="fwb fs12">${data.tieude}</h1>
+  <li>${data.noidung}</li>
+  <div class="">
+      <div id="#boxEdit" class="pa15 edit" style="background-color: rgb(214, 214, 214);">
+          <div class="fs12 fwb">Ví dụ</div>
+          <div class="bgcf w1 pa15">            
+              <code>${data.vidu}</code>
+          </div>
+          <button class="try-Button mt15 pa15 bra10 fwb">Hãy Tự Mình Thử</button>
+      </div>
+  </div>
+</div>`;
+});
+let htmlBasic = document.querySelector(".content-html");
+htmlBasic.innerHTML = tagHtml.join("");
 
-menuCode.forEach((element, index) => {
-  element.addEventListener("click", () => {
-    textarea(index);
+// toggle edit code
+let editCode = document.querySelector(".edit-code");
+let tryButton = document.querySelectorAll(".try-Button");
+
+tryButton.forEach((el, index) => {
+  el.addEventListener("click", () => {
+    editCode.classList.add("toggle");
+    document.querySelector('.close').addEventListener("click",()=>{
+      if(editCode.classList.contains("toggle")) {
+        editCode.classList.remove("toggle")
+      }
+    })
   });
 });
 
+
+let menuCode = document.querySelectorAll(".menuCode");
+
+menuCode.forEach((element, index) => {
+  if (index === 0) {
+    element.style.backgroundColor = "#636468";
+  }
+  element.addEventListener("click", () => {
+    textarea(index); // select ô nhập tương ứng vs menu
+
+    element.style.backgroundColor = "#636468"; // đổi màu background
+    menuCode.forEach((otherElement, otherIndex) => {
+      if (otherIndex !== index) {
+        otherElement.style.backgroundColor = "#383838";
+      }
+    });
+  });
+});
+
+let inputCode = document.querySelectorAll(".input-code");
 function textarea(index) {
   inputCode.forEach(function (element) {
     element.style.display = "none";
@@ -16,13 +59,18 @@ function textarea(index) {
 }
 textarea(0);
 
-function run() {
-  let htmlCode = document.getElementById("html-code").value;
-  let cssCode = document.getElementById("css-code").value;
-  let jsCode = document.getElementById("js-code").value;
-  let outputCode = document.getElementById("output");
+// edit code
+let runCode = document.querySelectorAll(".Run-code");
 
-  outputCode.contentDocument.body.innerHTML = htmlCode + "<style>" + cssCode + "</style>";
-  outputCode.contentWindow.eval(jsCode)
+runCode.forEach((el) => {
+  el.addEventListener("click", () => {
+    let htmlCode = document.getElementById("html-code").value;
+    let cssCode = document.getElementById("css-code").value;
+    let jsCode = document.getElementById("js-code").value;
+    let outputCode = document.getElementById("output");
 
-}
+    outputCode.contentDocument.body.innerHTML =
+      htmlCode + "<style>" + cssCode + "</style>";
+    outputCode.contentWindow.eval(jsCode);
+  });
+});
